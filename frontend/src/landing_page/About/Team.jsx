9ea30b58c333fd02bebb40dee78ecd5318b89df2
vaibhav sanmani <vaibhavsanmani9@gmail.com>
2026-05-12 22:39:53 +0530

    Refactor Team component for improved layout
    Refactor Team component layout and styling for better structure and readability.

--- frontend/src/landing_page/About/Team.jsx
+++ frontend/src/landing_page/About/Team.jsx
@@ -1,37 +1,73 @@
-import React from 'react'
+import React from "react";
+
 function Team() {
-    return ( 
-         <div className="container mb-5 mt-5">
-            <div className="row">
-                <h4 className=' mb-5 text-center'>People</h4>
-                <div className="col-1"></div>
-                <div className="col-5 text-center">
-                   <img src="./images/Owner.png" style={{width:"65%",borderRadius:"50%"}}/>
-                   <h4 className='mt-4'>Asmita Bagade</h4>
-                   <p className='text-muted mt-3'>Founder, CEO</p>
-                </div>
-                <div className="col-5 people" >
-                    <p >We started Mysa Arts with a vision to express creativity without limits,<br/>
-                        turning ideas into visually compelling stories through design and art.<br/>
-                        Today, Mysa Arts is steadily building its identity in the modern creative space.<br/>
-                    </p>
+  return (
+    <div className="container py-5">
+      <div className="row align-items-center">
 
-                    <p>We actively engage with the creative community and stay aligned<br/>
-                       with evolving design trends and modern visual culture,<br/>
-                       constantly refining our approach to stay relevant and impactful.<br/> 
-                    </p>
-                    <p>Creating art is our zen.<br/> </p>
+        <h4 className="text-center mb-5">People</h4>
 
-                    <p className='mb-5'>Connect on&nbsp; 
-                        <a href="/"  style={{textDecoration:"none"}}>Instagram</a> 
-                         / <a href="/"  style={{textDecoration:"none"}}>Trading QnA</a> 
-                         / <a href="/"  style={{textDecoration:"none"}}>Twitter</a><br/> </p>
-                    
-                </div>
-                <div className="col-1"></div>
-            </div>
+        {/* LEFT SPACE */}
+        <div className="col-lg-1"></div>
+
+        {/* IMAGE SECTION */}
+        <div className="col-12 col-lg-5 text-center mb-5 mb-lg-0">
+          <img
+            src="./images/Owner.png"
+            alt="Owner"
+            className="img-fluid"
+            style={{
+              width: "260px",
+              height: "260px",
+              objectFit: "cover",
+              borderRadius: "50%",
+            }}
+          />
+
+          <h4 className="mt-4">Asmita Bagade</h4>
+
+          <p className="text-muted mt-2">Founder, CEO</p>
         </div>
-     );
+
+        {/* TEXT SECTION */}
+        <div className="col-12 col-lg-5 text-center text-lg-start">
+          <p className="mb-4" style={{ lineHeight: "1.8" }}>
+            We started Mysa Arts with a vision to express creativity without
+            limits, turning ideas into visually compelling stories through
+            design and art. Today, Mysa Arts is steadily building its identity
+            in the modern creative space.
+          </p>
+
+          <p className="mb-4" style={{ lineHeight: "1.8" }}>
+            We actively engage with the creative community and stay aligned with
+            evolving design trends and modern visual culture, constantly
+            refining our approach to stay relevant and impactful.
+          </p>
+
+          <p className="mb-4">Creating art is our zen.</p>
+
+          <p className="mb-0">
+            Connect on&nbsp;
+            <a href="/" style={{ textDecoration: "none" }}>
+              Instagram
+            </a>
+            {" / "}
+            <a href="/" style={{ textDecoration: "none" }}>
+              Trading QnA
+            </a>
+            {" / "}
+            <a href="/" style={{ textDecoration: "none" }}>
+              Twitter
+            </a>
+          </p>
+        </div>
+
+        {/* RIGHT SPACE */}
+        <div className="col-lg-1"></div>
+
+      </div>
+    </div>
+  );
 }
 
 export default Team;
